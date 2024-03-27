3681a867a3dcf7c88ab8b1a5a58841df236c9d94
LeoChen <leo_chen@mayohr.com>
2024-03-27 16:58:50 +0800

    add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { Button } from '@/components/ui/button';
+import { ThemeProvider } from '@/components/theme-provider';
+import { ModeToggle } from '@/components/mode-toggle';
 
-export default function Home() {
+export default function Home({ children }: { children: React.ReactNode }) {
 	return (
-		<div>
-			<Button>Click me</Button>
-		</div>
+		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+			{children}
+			<ModeToggle />
+		</ThemeProvider>
 	);
 }
